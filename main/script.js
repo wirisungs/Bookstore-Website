99320552dea8841d875f37874f21b95fa4f6df75
Willis "Thiên Phúc" Crympt <phucplaymc@gmail.com>
2023-10-08 13:28:10 +0700

    ignored

--- main/script.js
+++ main/script.js
@@ -16,3 +16,27 @@ function copyMenu(){
     topPlace.innerHTML = topNav.innerHTML;
 }
 copyMenu();
+
+//show mobile menu
+const menuButton = document.querySelector('.trigger'),
+    closeButton = document.querySelector('.t-close'),
+    addclass = document.querySelector('.site');
+menuButton.addEventListener('click', function() {
+    addclass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click', function(){
+    addclass.classList.remove('showmenu')
+})
+
+
+
+//show sub menu on mobile
+const submenu = document.querySelectorAll('.has-child .icon small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null)
+    if (this.closet('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand')
+}
